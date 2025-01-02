69612903919dc6977c4954e271a7042cd1789ad0
pbenaja <benaja-phoba.phoba.1@ulaval.ca>
2025-01-01 21:10:11 -0500

    first commit

---
+++ WindowsForms-note_etudiant/FormPrincipale.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsForms_note_etudiant
+{
+    partial class FormPrincipale
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajouterLesNotesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajouterLesNotesToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // ajouterLesNotesToolStripMenuItem
+            // 
+            this.ajouterLesNotesToolStripMenuItem.Name = "ajouterLesNotesToolStripMenuItem";
+            this.ajouterLesNotesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ajouterLesNotesToolStripMenuItem.Text = "Ajouter les notes";
+            this.ajouterLesNotesToolStripMenuItem.Click += new System.EventHandler(this.ajouterLesNotesToolStripMenuItem_Click);
+            // 
+            // FormPrincipale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormPrincipale";
+            this.Text = "Interface Principale";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajouterLesNotesToolStripMenuItem;
+    }
+}
+
---
+++ WindowsForms-note_etudiant/FormPrincipale.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_note_etudiant
+{
+    public partial class FormPrincipale : Form
+    {
+        public FormPrincipale()
+        {
+            InitializeComponent();
+        }
+
+        private void ajouterLesNotesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormAjouterNote formAjouterNote = new FormAjouterNote();
+            formAjouterNote.MdiParent= this;
+            formAjouterNote.Show();
+        }
+    }
+}
